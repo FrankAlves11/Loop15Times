9711d3e8dc32707302fd70b3f1a028f67706a078
FrankAlves11 <Frankie.alves@icloud.com>
2023-08-30 09:04:07 -0700

    made changes

---
+++ 15Times.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+
+	{
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.writeLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}"):
+            
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}"):
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
